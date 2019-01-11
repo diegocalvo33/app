23dbef5205c5da56bb22af040add60737f23c0b0
diegocalvo33 <juan_diego36@hotmail.com>
2019-01-11 12:11:18 -0600

    test
    test

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var app = require( 'express' )();
 var http = require( 'http' ).Server( app );
 var io = require( 'socket.io' )( http );
 var port = process.env.PORT || 3000;
+var mysql = require( 'mysql' );
+var con = mysql.createConnection({
+    host: '',
+    user: '',
+    password: '',
+    database: ''
+});
+
+con.connect(function( err ){
+    if ( err ) throw err;
+});
 
 app.get( '/', function( req, res ) {
     res.sendFile( __dirname + '/index.html' );
@@ -10,6 +21,7 @@ app.get( '/', function( req, res ) {
 io.on( 'connection', function( socket ) {
     socket.on( 'chat message', function( msg ) {
         io.emit( 'chat message', msg );
+        con.query( 'INSERT INTO msg ( msg ) VALUES ( "'+msg+'" )' );
     });
 });
 
